aaf4c511bd6c7125e9adac90750a856ba4e207b3
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-09-13 09:24:42 +0530

    Styling an Element.

--- Master DOM Manipulation/app.js
+++ Master DOM Manipulation/app.js
@@ -1,37 +1,60 @@
-// DOM Manipulation
+// // DOM Manipulation
 
-//To Manipulate an Element in a DOM we have to be
-// able to select that Element.
-//We can do that by the following Methods.
+// //To Manipulate an Element in a DOM we have to be
+// // able to select that Element.
+// //We can do that by the following Methods.
 
-// GetElementById()
+// // GetElementById()
 
-const title = document.getElementById("sports");
-console.log("By ID");
+// const title = document.getElementById("sports");
+// console.log("By ID");
+// console.log(title);
+
+// // GetElementByClassName()
+
+// const listItem = document.getElementsByClassName("list-items");
+// console.log("By Class Name");
+// console.log(listItem);
+
+// // GetElementsByTagName()
+
+// const items = document.getElementsByTagName("li");
+// console.log("By Tag Name");
+// console.log(items);
+
+// // querySelector()
+
+// // Matches the first item with the selector it has been given
+// // Can be used with any Selector
+
+// const list = document.querySelector("ul");
+// console.log("querySelector : ");
+// console.log(list);
+
+// // querySelectorAll()
+
+// const lists = document.querySelectorAll("ul");
+// console.log("querySelectorAll : ");
+// console.log(lists);
+// // Returns a NodeList of all matching elements that matches that specific selector.
+
+// --------------------
+
+//Styling an Element
+
+const title = document.querySelector("#sports");
 console.log(title);
 
-// GetElementByClassName()
+title.style.color = "red";
 
-const listItem = document.getElementsByClassName("list-items");
-console.log("By Class Name");
-console.log(listItem);
+const heading = document.querySelector("#main-heading");
+heading.style.color = "orangered";
+heading.style.fontSize = "4rem";
 
-// GetElementsByTagName()
+console.log(heading);
 
-const items = document.getElementsByTagName("li");
-console.log("By Tag Name");
-console.log(items);
+const listItems = document.querySelectorAll(".list-items");
 
-// querySelector()
-// Matches the first item with the selector it has been given
-// Can be used with any Selector
-
-const list = document.querySelector("ul");
-console.log("querySelector : ");
-console.log(list);
-
-// querySelectorAll()
-
-const lists = document.querySelectorAll("ul");
-console.log("querySelectorAll : ");
-console.log(lists);
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontSize = "3rem";
+}
